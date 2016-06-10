145eababa27448496e680d6d5b1b477f1f8f6994
David Guan <DavidGuanDev@gmail.com>
2016-06-10 16:10:50 +0800

    Add special(π) button.

--- ios-calculator/ViewController.swift
+++ ios-calculator/ViewController.swift
@@ -11,9 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
+    var labelNeedClear = true
+    
+    @IBAction func performOperaton(sender: UIButton) {
+        labelNeedClear = true
+        if let mathSymbol = sender.currentTitle {
+            if mathSymbol == "π" {
+                resultLabel.text = String(M_PI)
+            }
+        }
+    }
     
     @IBAction func onTouch(sender: UIButton) {
-        print(resultLabel.text!)
+       
+        var displayText = resultLabel.text!
+        if labelNeedClear {
+            labelNeedClear = false
+            displayText = sender.currentTitle!
+        } else {
+            displayText += sender.currentTitle!
+        }
+        
+        if sender.currentTitle! == "π" {
+            resultLabel.text = String(M_PI)
+            return
+        }
+        
+        resultLabel.text = displayText
     }
 }
 
